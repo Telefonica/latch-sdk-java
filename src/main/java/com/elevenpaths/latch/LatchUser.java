070b9dda9ef60136efe96059e84a12abdf43d5a3
Davidcallegalindo <david.delacallegalindo@gmail.com>
2023-02-16 11:30:03 +0100

    delete spain sl

--- src/main/java/com/elevenpaths/latch/LatchUser.java
+++ src/main/java/com/elevenpaths/latch/LatchUser.java
@@ -1,5 +1,5 @@
 /*Latch Java SDK - Set of  reusable classes to  allow developers integrate Latch on their applications.
-Copyright (C) 2023 Telefonica Digital España S.L.
+Copyright (C) 2023 Telefonica Digital
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
